09fd565884067945f38471c7a54d9b6eb8342273
Gihan Ubayawardana <gihanubayawardana@gmail.com>
2025-06-16 21:31:19 +0800

    add components

---
+++ src/components/ButtonGroup.jsx
@@ -0,0 +1,18 @@
+import Button from "./Button";
+const secondaryButtons = [
+  "Mark all as complete",
+  "Mark all as incomplete",
+  "Reset to initial",
+  "Remove all items",
+];
+export default function ButtonGroup() {
+  return (
+    <section className="button-group">
+      {secondaryButtons.map((buttonText, index) => (
+        <Button key={index} type="secondary">
+          {buttonText}
+        </Button>
+      ))}
+    </section>
+  );
+}
